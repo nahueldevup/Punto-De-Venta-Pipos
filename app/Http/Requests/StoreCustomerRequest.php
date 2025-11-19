6592485c228996c82839563a47bc999e9bfdaa28
nahuel <cuenta3fne@gmail.com>
2025-11-19 20:27:37 -0300

    feat(customers): validaciones de clientes
    - Validación de nombre requerido
    - Teléfono opcional (máx 20 caracteres)

---
+++ app/Http/Requests/StoreCustomerRequest.php
@@ -0,0 +1,27 @@
+<?php
+//php artisan make:request StoreCustomerReques
+namespace App\Http\Requests;
+use Illuminate\Foundation\Http\FormRequest;
+class StoreCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; // Asumimos que cualquier usuario puede crear clientes
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'phone' => 'nullable|string|max:20',
+        ];
+    }
+}
